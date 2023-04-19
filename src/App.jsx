e803fe320e460e4c5b8fbb7ce1e9cf6655516a4c
caolan <caolan@digigrow.ie>
2023-04-19 12:10:11 -0400

    setting up validation state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./index.scss";
 
 //Components
 import Register from "./components/register";
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
   faCheck,
   faTimes,
@@ -7,8 +7,25 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 //REGEX VALIDATION
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,24}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
   return <div>Register!</div>;
 };
 
