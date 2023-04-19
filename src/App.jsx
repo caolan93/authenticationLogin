209e2f86d0fe298620448114d07acb1945a2816a
caolan <caolan@digigrow.ie>
2023-04-19 11:53:02 -0400

    intial setup for auth login

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect, useRef } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+//Components
+import Register from "./components/register";
+
+function App() {
+  return (
+    <main className="App">
+      <Register />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/register.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+//REGEX VALIDATION
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
